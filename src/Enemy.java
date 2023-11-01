10756dfc5743aad6142682e628969d5a717e52f1
BT_4N2_21 <a@a>
2023-11-01 14:05:23 -0400

    a

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,19 +1,44 @@
 public class Enemy {
-    private int difficultyMulti;
+    private double difficultyMulti;
     private int dungeonFloor;
-    private int enemyHealth;
-    private int enemyAttack;
+    private double enemyHealth;
+    private double enemyAttack;
     private String enemyName;
 
     public Enemy(int difficultyMulti, int dungeonFloor) {
             this.difficultyMulti = difficultyMulti;
             this.dungeonFloor = dungeonFloor;
-            if (dungeonFloor % 5 == 0) {
-                int enemyChooser = (int) (1 + (Math.random() * 3));
-
+        int enemyChooser = (int) (1 + (Math.random() * 3));
+        if (dungeonFloor % 5 == 0) {
+            if (enemyChooser == 1) {
+                enemyName = "Goblin King (BOSS)";
+                enemyHealth = 500 * difficultyMulti * (dungeonFloor / 5.0);
+                enemyAttack = 50 * difficultyMulti * (1 + dungeonFloor / 5.0 * 0.3);
+            } else if (enemyChooser == 2) {
+                enemyName = "Slime Queen (BOSS)";
+                enemyHealth = 750 * difficultyMulti * (dungeonFloor / 5.0);
+                enemyAttack = 30 * difficultyMulti * (1 + dungeonFloor / 5.0 * 0.3);
             } else {
-                int enemyChooser = (int) (1 + (Math.random() * 3));
+                enemyName = "Giant Gollum (BOSS)";
+                enemyHealth = 1000 * difficultyMulti * (dungeonFloor / 5.0);
+                enemyAttack = 40 * difficultyMulti * (1 + dungeonFloor / 5.0 * 0.3);
             }
+
+        } else {
+            if (enemyChooser == 1) {
+                enemyName = "Goblin";
+                enemyHealth = 500 * difficultyMulti * (dungeonFloor / 5.0);
+                enemyAttack = 50 * difficultyMulti * (1 + dungeonFloor / 5.0 * 0.3);
+            } else if (enemyChooser == 2) {
+                enemyName = "Slime";
+                enemyHealth = 750 * difficultyMulti * (dungeonFloor / 5.0);
+                enemyAttack = 30 * difficultyMulti * (1 + dungeonFloor / 5.0 * 0.3);
+            } else {
+                enemyName = "Golem";
+                enemyHealth = 1000 * difficultyMulti * (dungeonFloor / 5.0);
+                enemyAttack = 40 * difficultyMulti * (1 + dungeonFloor / 5.0 * 0.3);
+            }
+        }
     }
 
 
